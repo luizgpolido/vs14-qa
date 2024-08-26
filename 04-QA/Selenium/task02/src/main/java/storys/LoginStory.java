958e9f33964d8fd4bbb3a105734c381a9d186618
Mateus-de-Morais-Barros <mateusdemorais1245@gmail.com>
2024-08-26 08:47:34 -0300

    add test CT229

--- 04-QA/Selenium/task02/src/main/java/storys/LoginStory.java
+++ 04-QA/Selenium/task02/src/main/java/storys/LoginStory.java
@@ -7,8 +7,6 @@ public class LoginStory {
             "Como usuário do sistema, desejo fazer login na aplicação quando inserir email e senha validos";
 
     public static final String CT210_LOGIN = "CT210 - Valida login de usuario com dados validos";
-    public static final String CT211_LOGIN = "CT211 - Valida recuperação de senha";
-    public static final String CT214_LOGIN = "CT214 - Tentativa de assinar newsletter com email inválido";
-    public static final String CT215_LOGIN = "CT215 - Tentar cadastrar usuario com email já cadastrado";
+    public static final String CT229_LOGIN = "CT229 - Tentativa de login de usuario com dados inválidos";
 
 }
